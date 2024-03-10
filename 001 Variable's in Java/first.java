5a059c19bfbd543771e8bb293dbb07e4fb423f72
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-03-10 10:53:41 +0530

    new

---
+++ 001 Variable's in Java/first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void main(String args[]) {
+        // Normal Print Statement.
+        System.out.print("Hello, Umair!...");
+        System.out.print("Hello world!");
+        // Print statement 'println' which print the next upcoming print command in next line.
+        System.out.println("Hello, Umair!...");
+        System.out.println("Hello world!");
+        // here we use '\n' -> it means move to the next line
+        System.out.print("Hello, Umair!...\n");
+        System.out.print("Hello world!\n");
+    }
+}
---
+++ 001 Variable's in Java/second.java
@@ -0,0 +1,15 @@
+// ****
+// ***
+// **
+// *
+public class second {
+    public static void main(String args[]){
+        int num = 4;
+        for (int index = num; index > 0; index--) {
+            for (int i = 0; i < index; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

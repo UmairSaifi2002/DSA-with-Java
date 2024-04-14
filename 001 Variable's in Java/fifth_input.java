7e5cb694aedeb64ab813bfedf0c72a9f2b307f55
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-04-14 12:46:52 +0530

    Update fifth_input.java

--- 001 Variable's in Java/fifth_input.java
+++ 001 Variable's in Java/fifth_input.java
@@ -13,7 +13,19 @@ public class fifth_input {
         System.out.println("Your stringTwo input -> " + input2);
 
         // Integer taken as an input
-        int num = sc.nextInt();
-        System.out.println("Your num input -> " + num);
+        int num1 = sc.nextInt();
+        System.out.println("Your Integer number input -> " + num1);
+
+        // Long taken as an input
+        long num2 = sc.nextLong();
+        System.out.println("Your Long number input -> " + num2);
+
+        // Floating number taken as an input
+        float num3 = sc.nextFloat();
+        System.out.println("Your Floating number input -> " + num3);
+
+        // Double number taken as an input
+        double num4 = sc.nextDouble();
+        System.out.println("Your Double number input -> " + num4);
     }
 }

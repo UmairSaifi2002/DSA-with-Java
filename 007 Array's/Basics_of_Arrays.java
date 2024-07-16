44082c8859ccddf9018a8b7dae95508c9aff3a1e
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-07-16 15:58:19 +0530

    Basics of Java

---
+++ 007 Array's/Basics_of_Arrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Basics_of_Arrays {
+
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n);
+        // Creation of an Array
+
+    }
+}

89b9e9d80bc382bbd546f08201881ab9abbd9cab
UmairSaifi2002 <umairsaifi2002@gmail.com>
2024-07-21 19:26:06 +0530

    New Commit

---
+++ 007 Array's/Buy_and_Sell_Stocks.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Buy_and_Sell_Stocks {
+
+    public static int max_profit(int arr[]){
+        // so we can tackle this problem by making an array which tells the profit of that day
+        // if you sell the stock on that day
+        // int profit[] = new int[arr.length];
+        // profit[0] = 0; // because at the first day you only buy the stock donot sell
+        // int min = arr[0];
+        // for (int i = 1; i < arr.length; i++) {
+        //     for (int j = 0; j < i; j++) min = min > arr[j] ? arr[j] : min ;
+        //     profit[i] = (arr[i] - min) > 0 ? (arr[i] - min) : 0;
+        // }
+        // int max = 0;
+        // for (int i = 0; i < profit.length; i++){ 
+        //     max = max < profit[i] ? profit[i] : max ;
+        //     // System.out.print(profit[i]);
+        // }
+        // return max;
+
+        // ************************* By Teacher ********************
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (buyPrice < arr[i]) { // Profit
+                int profit = arr[i] - buyPrice; // Today's Profit.
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyPrice = arr[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // You are given an array prices where prices[i] is the price of a given stock
+        // on the ith day. You want to maximize your profit by choosing a single day to
+        // buy one stock and choosing a different day in the future to sell that stock.
+        // return the maximum profit you can achieve from this transection. if you
+        // cannot achieve any profit, return 0.
+        int price[] = {7,1,5,3,6,4}; 
+        int result = max_profit(price);
+        System.out.println("The max price is "+result);
+    }
+}
--- 007 Array's/Trapping_RainWater_Problem.java
+++ 007 Array's/Trapping_RainWater_Problem.java
@@ -4,29 +4,60 @@ public class Trapping_RainWater_Problem {
 
     public static int Trap_Rainwater(int arr[]) {
 
+        // ***************************** By Me ****************************
         // here we are making arrays which stores right and left highest boundry
-        int right[] = new int[arr.length];
-        int left[] = new int[arr.length];
-        right[0] = 0;
-        left[0] = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < i; j++)  right[i] = right[i] < arr[j] ? arr[j] : right[i];
-            for (int j = i; j < left.length; j++)  left[i] = left[i] < arr[j] ? arr[j] : left[i];
-        }
-        // for (int i = 0; i < right.length; i++) System.out.print(right[i]+" ");
+        // int n = arr.length;
+        // int right[] = new int[n];
+        // int left[] = new int[n];
+        // right[n - 1] = arr[n - 1];
+        // left[0] = 0;
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < i; j++)  right[i] = right[i] < arr[j] ? arr[j] : right[i];
+        //     for (int j = i; j < n; j++)  left[i] = left[i] < arr[j] ? arr[j] : left[i];
+        // }
+
+        // for (int i = 0; i < n; i++) System.out.print(right[i]+" ");
         // System.out.println();
-        // for (int i = 0; i < left.length; i++) System.out.print(left[i]+" ");
+        // for (int i = 0; i < n; i++) System.out.print(left[i]+" ");
 
         // now we gonna calculate the trapped water 
+        // water_level = ((Math.min(right[i], left[i])) - arr[i]);
         // we are using formula :- trap_water = water level - height of a current bar
 
-        int water_level = 0;
+        // int water_level = 0;
+        // int trapped_water = 0;
+        // for (int i = 0; i < n; i++) {
+        //     water_level = ((Math.min(right[i], left[i])) - arr[i]);
+        //     if (water_level > 0) {
+        //         trapped_water += water_level ;
+        //     } 
+        // }
+
+        // return trapped_water;
+
+        
+        // ***************************** By Teacer ****************************
+        int n = arr.length;
+
+        // Calculate left most boundary - array
+        int leftMax[] = new int[n];
+        leftMax[0] = arr[0];
+        for (int i = 0; i < n; i++) leftMax[i] = Math.max(arr[i], leftMax[i-1]);
+
+        // calculate right most boundary - array
+        int rightMax[] = new int[n];
+        rightMax[n-1] = arr[n-1];
+        for (int i = n-2; i > 0; i--) rightMax[i] = Math.max(arr[i], rightMax[i+1]);
+
         int trapped_water = 0;
-        for (int i = 0; i < arr.length; i++) {
-            water_level = ((Math.min(right[i], left[i])) - arr[i]);
-            if (water_level > 0) {
-                trapped_water += water_level ;
-            } 
+        int water_level;
+        // loop
+        for (int i = 0; i < n; i++) {
+            // water_level = min(leftmax bound, rightmax bound)
+            water_level = Math.min(leftMax[i], rightMax[i]);
+            // trapped_water += water_level - arr[i]
+            trapped_water += water_level - arr[i];
         }
 
         return trapped_water;

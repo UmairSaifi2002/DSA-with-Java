dd7eb7aeb03e5fd47ed34b142ca0df21d1f357b0
Umair Saifi <umairsaifi2002@gmail.com>
2024-08-27 11:25:48 +0530

    New Commit

--- 017 Linked List/Basics_of_LinkedList.java
+++ 017 Linked List/Basics_of_LinkedList.java
@@ -1,65 +1,83 @@
 public class Basics_of_LinkedList {
-
     public static void main(String[] args) {
+        // Create a new LinkedList object
         LinkedList ll = new LinkedList();
+
+        // Add elements to the LinkedList
         ll.addFirst(3);
         ll.addFirst(2);
         ll.addFirst(1);
         ll.addLast(4);
         ll.addLast(5);
         ll.addLast(6);
+
+        // Print the LinkedList
         ll.print();
     }
 }
 
-class LinkedList{
-    public static class Node{
+class LinkedList {
+    // Define a static inner class Node to represent each element in the LinkedList
+    public static class Node {
         int data;
         Node next;
+
         public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
+    // Declare Head and Tail references to keep track of the LinkedList
     public static Node Head; // Creating a Head
     public static Node Tail; // Creating a Tail
 
+    // Method to add a new element at the beginning of the LinkedList
     public void addFirst(int data) {
-        // Step 1 : Create new Node
+        // Step 1: Create a new Node with the given data
         Node newNode = new Node(data);
 
-        if (Head == null) { // if there is no one node is present
+        // If the LinkedList is empty, set both Head and Tail to the new Node
+        if (Head == null) {
             Head = Tail = newNode;
             return;
         }
-        //Step 2 : newNode next = head
-        newNode.next = Head; // Linking our newNode to head of the linked list
-        // Step 3 : head = newNode
+
+        // Step 2: Set the new Node's next reference to the current Head
+        newNode.next = Head;
+
+        // Step 3: Update the Head reference to point to the new Node
         Head = newNode;
     }
 
-    public void addLast(int data){
-        // Step 1 :- Create new Node
+    // Method to add a new element at the end of the LinkedList
+    public void addLast(int data) {
+        // Step 1: Create a new Node with the given data
         Node newNode = new Node(data);
 
-        if (Head == null) { // if there is no one node is present
+        // If the LinkedList is empty, set both Head and Tail to the new Node
+        if (Head == null) {
             Head = Tail = newNode;
             return;
         }
 
-        // Step 2 :- newNode next = null
-        Tail.next = newNode; // changing Tail
+        // Step 2: Set the current Tail's next reference to the new Node
+        Tail.next = newNode;
 
-        // Step 3:- Tail = newNode
+        // Step 3: Update the Tail reference to point to the new Node
         Tail = newNode;
     }
 
-    public void print(){
+    // Method to print the elements of the LinkedList
+    public void print() {
+        // Start from the Head and traverse the LinkedList until the end
         Node temp = Head;
         while (temp != null) {
-            System.out.print(temp.data+" -> ");
+            System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
         System.out.print("null");
     }
 }
+
+

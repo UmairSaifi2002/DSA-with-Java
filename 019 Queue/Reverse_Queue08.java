aa702270a5f975ffd0fd67b9a2487ced7c00b464
Umair Saifi <umairsaifi2002@gmail.com>
2024-09-18 23:47:21 +0530

    Some new Question About Queue's

---
+++ 019 Queue/Reverse_Queue08.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Reverse_Queue08 {
+
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+        System.out.println(q);
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        reverse(q);
+    }
+}
